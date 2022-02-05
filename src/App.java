ae816f77b037e180e6bf58cef6d8e5c3c2baa28e
Majd <1aburasmaj@hdsb.ca>
2022-02-05 13:56:03 -0500

    Implemented CSV Wrtier

--- src/App.java
+++ src/App.java
@@ -4,9 +4,10 @@ import java.util.List;
 public class App {
 
     static List<List<Double>> data = new ArrayList<>();
+    static String[] headers;
 
     public static void main(String[] args) throws Exception {
-        data = CSVReader.convertToArrayList("/Users/Majdp/Desktop/test.csv");
+        data = CSVReader.convertToArrayList("C:/Users/Driver Station/Desktop/test4.csv");
         long startTime = System.currentTimeMillis();
         long currentTime;
         for (int i = 0; i < data.size(); i++) {
@@ -16,6 +17,12 @@ public class App {
             
             System.out.printf("%s | %s,%s\n",currentTime,data.get(i).get(0).longValue(),data.get(i).get(1));
         }
+        // data = CSVReader.convertToArrayList("C:/Users/Driver Station/Desktop/test2.csv");
+        // CSVWriter writer = new CSVWriter("C:/Users/Driver Station/Desktop/");
+        // writer.setFileName("test4");
+        // writer.setHeader("test,motor");
+        // writer.importData(data);
+        // writer.output();
     }
         
 }
